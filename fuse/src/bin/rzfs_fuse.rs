b6b7ca8fa6bb3bcbd7dd1b1c09c159c1451808ef
Jan Kasiak <jan@cybojanek.net>
2024-04-06 11:44:35 -0400

    Avoid RZFSFuse never constructed warning

--- fuse/src/bin/rzfs_fuse.rs
+++ fuse/src/bin/rzfs_fuse.rs
@@ -5,11 +5,14 @@ use fuser::Filesystem;
 use std::error::Error;
 use zfs::phys;
 
+#[derive(Debug)]
 struct RZFSFuse;
 
 impl Filesystem for RZFSFuse {}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let rzfs = RZFSFuse {};
+    println!("{:?}", rzfs);
     println!("{}", phys::SECTOR_SHIFT);
 
     Ok(())
