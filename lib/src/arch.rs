c044b130467a256df5e2253d78eee5fef72d0a9e
Jan Kasiak <jan@cybojanek.net>
2024-04-06 22:13:16 -0400

    Implement SSE2 fletcher4

---
+++ lib/src/arch.rs
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0 OR MIT
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub(crate) mod x86_any;
---
+++ lib/src/arch/x86_any.rs
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0 OR MIT
+
+#[cfg(target_arch = "x86")]
+use core::arch::x86 as arch;
+
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64 as arch;
+
+////////////////////////////////////////////////////////////////////////////////
+
+const LEAF_PROCESSOR_INFO_AND_FEATURE_BITS: u32 = 1;
+
+const MASK_LEAF_PIAFB_EDX_SSE_2: u32 = 1 << 26;
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Is SSE2 supported by the CPU.
+pub(crate) fn is_sse2_supported() -> bool {
+    unsafe {
+        // Get number of leaves.
+        let (leaf_max, _) = arch::__get_cpuid_max(0);
+        if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
+            return false;
+        }
+
+        let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
+
+        (cpuid.edx & MASK_LEAF_PIAFB_EDX_SSE_2) != 0
+    }
+}
