b479ed60293040b8a95f4248a006e616dfacc469
Jan Kasiak <jan@cybojanek.net>
2024-04-10 22:43:48 -0400

    Implement AVX2 fletcher4

--- lib/src/arch/x86_any.rs
+++ lib/src/arch/x86_any.rs
@@ -14,7 +14,45 @@ const MASK_LEAF_PIAFB_EDX_SSE_2: u32 = 1 << 26;
 
 const MASK_LEAF_PIAFB_ECX_SSSE_3: u32 = 1 << 9;
 
+const MASK_LEAF_PIAFB_ECX_AVX: u32 = 1 << 28;
+
 ////////////////////////////////////////////////////////////////////////////////
+
+const LEAF_EXTENDED_FEATURES: u32 = 7;
+
+const MASK_LEAF_EXFT_EBX_AVX_2: u32 = 1 << 5;
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Is AVX supported by the CPU.
+pub(crate) fn is_avx_supported() -> bool {
+    unsafe {
+        // Get number of leaves.
+        let (leaf_max, _) = arch::__get_cpuid_max(0);
+        if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
+            return false;
+        }
+
+        let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
+
+        (cpuid.ecx & MASK_LEAF_PIAFB_ECX_AVX) != 0
+    }
+}
+
+/// Is AVX2 supported by the CPU.
+pub(crate) fn is_avx2_supported() -> bool {
+    unsafe {
+        // Get number of leaves.
+        let (leaf_max, _) = arch::__get_cpuid_max(0);
+        if leaf_max < LEAF_EXTENDED_FEATURES {
+            return false;
+        }
+
+        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+        (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_2) != 0
+    }
+}
 
 /// Is SSE2 supported by the CPU.
 pub(crate) fn is_sse2_supported() -> bool {
