0ecf1ed5be7e189ca74068f99340ee8d55b141f3
Jan Kasiak <jan@cybojanek.net>
2024-05-23 22:45:25 -0400

    Add checksum label

--- lib/src/checksum.rs
+++ lib/src/checksum.rs
@@ -9,5 +9,8 @@ pub use fletcher2::{Fletcher2, Fletcher2Implementation};
 pub(crate) mod fletcher4;
 pub use fletcher4::{Fletcher4, Fletcher4Implementation};
 
+pub(crate) mod label;
+pub use label::{label_checksum, label_verify, LabelChecksumError, LabelVerifyError};
+
 pub(crate) mod sha256;
 pub use sha256::{Sha256, Sha256Implementation};
