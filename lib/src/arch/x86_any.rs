ab963cd3f642604066a099e3bcfb05570ac17683
Jan Kasiak <jan@cybojanek.net>
2026-03-11 22:27:37 -0400

    Fix lint warnings

--- lib/src/arch/x86_any.rs
+++ lib/src/arch/x86_any.rs
@@ -30,152 +30,132 @@ const MASK_LEAF_EXFT_EBX_AVX_512_BW: u32 = 1 << 30;
 
 /// Is AVX supported by the CPU.
 pub(crate) fn is_avx_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
-
-        (cpuid.ecx & MASK_LEAF_PIAFB_ECX_AVX) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
+
+    (cpuid.ecx & MASK_LEAF_PIAFB_ECX_AVX) != 0
 }
 
 /// Is AVX2 supported by the CPU.
 pub(crate) fn is_avx2_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_EXTENDED_FEATURES {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
-
-        (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_2) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_EXTENDED_FEATURES {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+    (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_2) != 0
 }
 
 /// Is AVX512F supported by the CPU.
 pub(crate) fn is_avx512f_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_EXTENDED_FEATURES {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
-
-        (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_512_F) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_EXTENDED_FEATURES {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+    (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_512_F) != 0
 }
 
 /// Is AVX512BW supported by the CPU.
 pub(crate) fn is_avx512bw_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_EXTENDED_FEATURES {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
-
-        (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_512_BW) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_EXTENDED_FEATURES {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+    (cpuid.ebx & MASK_LEAF_EXFT_EBX_AVX_512_BW) != 0
 }
 
 /// Is BMI1 supported by the CPU.
 pub(crate) fn is_bmi1_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_EXTENDED_FEATURES {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
-
-        (cpuid.ebx & MASK_LEAF_EXFT_EBX_BMI1) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_EXTENDED_FEATURES {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+    (cpuid.ebx & MASK_LEAF_EXFT_EBX_BMI1) != 0
 }
 
 /// Is BMI2 supported by the CPU.
 pub(crate) fn is_bmi2_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_EXTENDED_FEATURES {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
-
-        (cpuid.ebx & MASK_LEAF_EXFT_EBX_BMI2) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_EXTENDED_FEATURES {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+    (cpuid.ebx & MASK_LEAF_EXFT_EBX_BMI2) != 0
 }
 
 /// Is SHA supported by the CPU.
 pub(crate) fn is_sha_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_EXTENDED_FEATURES {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
-
-        (cpuid.ebx & MASK_LEAF_EXFT_EBX_SHA) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_EXTENDED_FEATURES {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_EXTENDED_FEATURES);
+
+    (cpuid.ebx & MASK_LEAF_EXFT_EBX_SHA) != 0
 }
 
 /// Is SSE2 supported by the CPU.
 pub(crate) fn is_sse2_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
-
-        (cpuid.edx & MASK_LEAF_PIAFB_EDX_SSE_2) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
+
+    (cpuid.edx & MASK_LEAF_PIAFB_EDX_SSE_2) != 0
 }
 
 /// Is SSE3 supported by the CPU.
 pub(crate) fn is_sse3_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
-
-        (cpuid.ecx & MASK_LEAF_PIAFB_ECX_SSE_3) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
+
+    (cpuid.ecx & MASK_LEAF_PIAFB_ECX_SSE_3) != 0
 }
 
 /// Is SSSE3 supported by the CPU.
 pub(crate) fn is_ssse3_supported() -> bool {
-    unsafe {
-        // Get number of leaves.
-        let (leaf_max, _) = arch::__get_cpuid_max(0);
-        if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
-            return false;
-        }
-
-        let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
-
-        (cpuid.ecx & MASK_LEAF_PIAFB_ECX_SSSE_3) != 0
+    // Get number of leaves.
+    let (leaf_max, _) = arch::__get_cpuid_max(0);
+    if leaf_max < LEAF_PROCESSOR_INFO_AND_FEATURE_BITS {
+        return false;
     }
+
+    let cpuid = arch::__cpuid(LEAF_PROCESSOR_INFO_AND_FEATURE_BITS);
+
+    (cpuid.ecx & MASK_LEAF_PIAFB_ECX_SSSE_3) != 0
 }
 
 ////////////////////////////////////////////////////////////////////////////////
